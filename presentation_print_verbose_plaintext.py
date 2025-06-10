33403b952ebe669ff1e7ef83efcb6581267960b5
Kris Bahnsen <Kris@KBEmbedded.com>
2025-06-09 18:25:21 -0700

    print_verbose: ability to not print talks which are not accepted
    Signed-off-by: Kris Bahnsen <Kris@KBEmbedded.com>

--- presentation_print_verbose_plaintext.py
+++ presentation_print_verbose_plaintext.py
@@ -23,39 +23,40 @@ with open(sys.argv[1], 'r') as csvfile:
   csvlines=csv.DictReader(csvfile, delimiter=',');
   print("______________________________________________________________________________")
   for row in csvlines:
-    name = row['Name'];
-    addnames = row['Additional Names'];
-    social_media = row['Social Media'];
-    website = row['Website'];
-    email = row['Email'];
-    pres_format = row['Format'];
-    length = row['Length'];
-    title = row['Title'];
-    abstract = row['Abstract'];
-    outline = row['Outline'];
-    bio = row['Bio'];
-    print("NAME:");
-    if addnames:
-      print("%s, %s\n" % (name, addnames));
-    else:
-      print("%s\n" % name);
-    print("SOCIAL MEDIA:")
-    print("%s\n" % social_media);
-    print("WEBSITE:")
-    print("%s\n" % website);
-    print("EMAIL (Don't share outside of HHV/SSV/DEF CON!):")
-    print("%s\n" % email);
-    print("FORMAT:")
-    print("%s\n" % pres_format);
-    print("DURATION:")
-    print("%s\n" % length);
-    print("TITLE:")
-    print("%s\n" % title);
-    print("ABSTRACT:")
-    print("%s\n" % abstract);
-    print("DETAILED OUTLINE:")
-    print("%s\n" % outline);
-    print("BIO:")
-    print("%s\n" % bio);
-    print("______________________________________________________________________________")
+    if row['Accepted'].lower() != "n":
+      name = row['Name'];
+      addnames = row['Additional Names'];
+      social_media = row['Social Media'];
+      website = row['Website'];
+      email = row['Email'];
+      pres_format = row['Format'];
+      length = row['Length'];
+      title = row['Title'];
+      abstract = row['Abstract'];
+      outline = row['Outline'];
+      bio = row['Bio'];
+      print("NAME:");
+      if addnames:
+        print("%s, %s\n" % (name, addnames));
+      else:
+        print("%s\n" % name);
+      print("SOCIAL MEDIA:")
+      print("%s\n" % social_media);
+      print("WEBSITE:")
+      print("%s\n" % website);
+      print("EMAIL (Don't share outside of HHV/SSV/DEF CON!):")
+      print("%s\n" % email);
+      print("FORMAT:")
+      print("%s\n" % pres_format);
+      print("DURATION:")
+      print("%s\n" % length);
+      print("TITLE:")
+      print("%s\n" % title);
+      print("ABSTRACT:")
+      print("%s\n" % abstract);
+      print("DETAILED OUTLINE:")
+      print("%s\n" % outline);
+      print("BIO:")
+      print("%s\n" % bio);
+      print("______________________________________________________________________________")
    
